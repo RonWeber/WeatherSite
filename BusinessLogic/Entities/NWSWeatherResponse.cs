a6def6b25bf37ae5609ed244e0800beb624c854e
Ron Weber <Ronnie.weber@yahoo.com>
2016-06-01 14:58:53 -0400

    Fixed minor HTML weirdness

--- BusinessLogic/Entities/NWSWeatherResponse.cs
+++ BusinessLogic/Entities/NWSWeatherResponse.cs
@@ -47,7 +47,8 @@ namespace BusinessLogic.Entities
                 table += "<td>" + information.data.timelayout[0].Items[i] + "</td>";
                 if (information?.data?.parameters?.conditionsicon?.iconlink[i/2] != null)
                     table += "<td>" + "<img class=\"weather_icon\" src=\"" + information.data.parameters.conditionsicon.iconlink[i / 2] + "\" />";
-                table += information.data.parameters.weather.weatherconditions[i / 2].weathersummary + "</td>";
+                if (information?.data?.parameters?.weather?.weatherconditions[i / 2]?.weathersummary != null)
+                    table += information.data.parameters.weather.weatherconditions[i / 2].weathersummary + "</td>";
                 table += "<td>" + information.data.parameters.temperature[1].value[i / 2] + "</td>";
                 table += "<td>" + information.data.parameters.temperature[0].value[i / 2] + "</td>";
                 table += "</tr>";
--- BusinessLogic/Entities/WeatherResponse.cs
+++ BusinessLogic/Entities/WeatherResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace BusinessLogic.Entities
 {
     public abstract class WeatherResponse
