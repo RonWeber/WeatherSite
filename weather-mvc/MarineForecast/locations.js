33a4a339fdbc972ecf58a8bdf208c12255f6d95f
Mike Weber <mikeweber03@yahoo.com>
2016-06-24 16:31:13 -0400

    Add a Marine Forecast page

---
+++ weather-mvc/MarineForecast/locations.js
@@ -0,0 +1,37 @@
+﻿
+//http://www.nws.noaa.gov/om/marine/gtlakes.htm
+var locations = [{
+    'area': 'Great Lakes',
+    'subArea':
+    [
+        {
+            'title': 'Lake Erie Offshore',
+            'forecasts':
+            [ 
+                { 'Description': 'Lake Erie', url: 'LEZ161/096161' },
+                { 'Description': 'Detroit River Light to Maumee Bay OH to Reno Beach OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ162/096162' },
+                { 'Description': 'Reno Beach to The Islands OH beyond 5 NM offshore to U.S./Canadian border ', url: 'LEZ163/096163' },
+                { 'Description': 'The Islands to Vermilion OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ164/096164' },
+                { 'Description': 'Vermilion to Avon Point OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ165/096165' },
+                { 'Description': 'Avon Point to Willowick OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ166/096166' },
+                { 'Description': 'Willowick to Geneva-on-the Lake OH beyond 5 NM offshore to U.S./Canadian border (', url: 'LEZ167/096167' },
+                { 'Description': 'Geneva-on-the-Lake to Conneaut OH beyond 5 NM offshore to U.S./Canadian border ', url: 'LEZ168/096168' },
+                { 'Description': 'Conneaut OH to Ripley NY beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ169/096169' },
+                { 'Description': 'Ripley to Buffalo NY extending from 5 NM offshore to U.S./Canadian border', url: 'LEZ061/096061' }        
+            ]
+        },
+        {
+            'title': 'Lake Ontario Offshore',
+            'forecasts':
+            [
+                { 'Description': 'Lake Ontario', url: 'LOZ061/097061' },
+                { 'Description': 'Niagara River to Hamlin Beach NY beyond 5 NM offshore to U.S./Canadian borde', url: 'LOZ062/097062' },
+                { 'Description': 'Hamlin Beach to Sodus Bay NY beyond 5 NM offshore to U.S./Canadian border', url: 'LOZ063/097063' },
+                { 'Description': 'Sodus Bay to Mexico Bay NY beyond 5 NM offshore to U.S./Canadian border', url: 'LOZ064/097064' },
+                { 'Description': 'Mexico Bay NY to the St. Lawrence River beyond 5 NM offshore to U.S./Canadian border', url: 'LOZ065/097065' }
+            ]
+        }
+    ]
+}]
+
+
---
+++ weather-mvc/MarineForecast/marine.js
@@ -0,0 +1,15 @@
+﻿var Weather = {};
+
+Weather.Marine = (function () {
+    function init() {
+        alert('init');
+    };
+
+    
+
+
+//Any functions declared in this 'return' function act like public functions 
+    return {
+        init: init
+    }
+})();
