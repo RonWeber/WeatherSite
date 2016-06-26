697e2d26e35df035c488721aefb3dfcac54787ac
Mike <mikeweber03@yahoo.com>
2016-06-25 21:39:46 -0400

    Add Marine Forecast

--- weather-mvc/MarineForecast/locations.js
+++ weather-mvc/MarineForecast/locations.js
@@ -1,36 +1,83 @@
 ﻿
 //http://www.nws.noaa.gov/om/marine/gtlakes.htm
 var locations = [{
-    'area': 'Great Lakes',
-    'subArea':
+    'title': 'Great Lakes',
+    'zones':
     [
         {
-            'title': 'Lake Erie Offshore',
-            'forecasts':
-            [ 
-                { 'Description': 'Lake Erie', url: 'LEZ161/096161' },
-                { 'Description': 'Detroit River Light to Maumee Bay OH to Reno Beach OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ162/096162' },
-                { 'Description': 'Reno Beach to The Islands OH beyond 5 NM offshore to U.S./Canadian border ', url: 'LEZ163/096163' },
-                { 'Description': 'The Islands to Vermilion OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ164/096164' },
-                { 'Description': 'Vermilion to Avon Point OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ165/096165' },
-                { 'Description': 'Avon Point to Willowick OH beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ166/096166' },
-                { 'Description': 'Willowick to Geneva-on-the Lake OH beyond 5 NM offshore to U.S./Canadian border (', url: 'LEZ167/096167' },
-                { 'Description': 'Geneva-on-the-Lake to Conneaut OH beyond 5 NM offshore to U.S./Canadian border ', url: 'LEZ168/096168' },
-                { 'Description': 'Conneaut OH to Ripley NY beyond 5 NM offshore to U.S./Canadian border', url: 'LEZ169/096169' },
-                { 'Description': 'Ripley to Buffalo NY extending from 5 NM offshore to U.S./Canadian border', url: 'LEZ061/096061' }        
+            'title': 'Lake Erie',
+            'subZones':
+            [{
+                'title': 'Offshore',
+                'forecasts':
+                [
+                    { 'Description': 'Lake Erie', 'url': '/great_lakes/LE/LEZ161.txt' },
+                    { 'Description': 'Detroit River Light to Maumee Bay OH to Reno Beach OH beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LE/LEZ162.txt' },
+                    { 'Description': 'Reno Beach to The Islands OH beyond 5 NM offshore to U.S./Canadian border ', 'url': '/great_lakes/LE/LEZ163.txt' },
+                    { 'Description': 'The Islands to Vermilion OH beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LE/LEZ164.txt' },
+                    { 'Description': 'Vermilion to Avon Point OH beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LE/LEZ165.txt' },
+                    { 'Description': 'Avon Point to Willowick OH beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LE/LEZ166.txt' },
+                    { 'Description': 'Willowick to Geneva-on-the Lake OH beyond 5 NM offshore to U.S./Canadian border (', 'url': '/great_lakes/LE/LEZ167.txt' },
+                    { 'Description': 'Geneva-on-the-Lake to Conneaut OH beyond 5 NM offshore to U.S./Canadian border ', 'url': '/great_lakes/LE/LEZ168.txt' },
+                    { 'Description': 'Conneaut OH to Ripley NY beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LE/LEZ169.txt' },
+                    { 'Description': 'Ripley to Buffalo NY extending from 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LE/LEZ061.txt' }
+                ]
+            },
+            {
+                'title': 'Michigan Near Shore',
+                'forecasts':
+                [
+                    { 'Description': 'Michigan Waters of Lake Erie from Detroit River to North Cape, MI', 'url': '/near_shore/LE/LEZ444.txt' }
+                ]
+            },
+            {
+                'title': 'Ohio Near Shore',
+                'forecasts':
+                [
+                    { 'Description': 'Maumee Bay to Reno Beach, OH', 'url': '/near_shore/LE/LEZ142.txt' },
+                    { 'Description': 'Reno Beach to The Islands, OH', 'url': '/near_shore/LE/LEZ143.txt' },
+                    { 'Description': 'The Islands to Vermilion, OH', 'url': '/near_shore/LE/LEZ144.txt' },
+                    { 'Description': 'Vermilion to Avon Point, OH', 'url': '/near_shore/LE/LEZ145.txt' },
+                    { 'Description': 'Avon Point to Willowick, OH', 'url': '/near_shore/LE/LEZ146.txt' },
+                    { 'Description': 'Willowick to Geneva-on-the-Lake, OH', 'url': '/near_shore/LE/LEZ147.txt' },
+                    { 'Description': 'Geneva-on-the-Lake to Conneaut, OH', 'url': '/near_shore/LE/LEZ148.txt' }
+                ]
+            },
+            {
+                'title': 'Pennsylvania Near Shore',
+                'forecasts':
+                [
+                    { 'Description': 'Conneaut, OH to Ripley, NY', 'url': '/near_shore/LE/LEZ149.txt' },
+                ]
+            },
+            {
+                'title': 'New York Near Shore',
+                'forecasts':
+                [
+                    { 'Description': 'Ripley to Dunkirk, NY', 'url': '/near_shore/LE/LEZ040.txt' },
+                    { 'Description': 'Dunkirk to Buffalo, NY', 'url': '/near_shore/LE/LEZ041.txt' },
+                    { 'Description': 'Upper Niagara River and Buffalo Harbor', 'url': '/near_shore/LE/LEZ020.txt' },
+                    { 'Description': 'Lower Niagara River', 'url': '/near_shore/LE/LEZ030.txt' },
+                ]
+            }
+
             ]
         },
         {
-            'title': 'Lake Ontario Offshore',
-            'forecasts':
-            [
-                { 'Description': 'Lake Ontario', url: 'LOZ061/097061' },
-                { 'Description': 'Niagara River to Hamlin Beach NY beyond 5 NM offshore to U.S./Canadian borde', url: 'LOZ062/097062' },
-                { 'Description': 'Hamlin Beach to Sodus Bay NY beyond 5 NM offshore to U.S./Canadian border', url: 'LOZ063/097063' },
-                { 'Description': 'Sodus Bay to Mexico Bay NY beyond 5 NM offshore to U.S./Canadian border', url: 'LOZ064/097064' },
-                { 'Description': 'Mexico Bay NY to the St. Lawrence River beyond 5 NM offshore to U.S./Canadian border', url: 'LOZ065/097065' }
-            ]
-        }
+            'title': 'Lake Ontario',
+            'subZones':
+            [{
+                'title': 'Lake Ontario Offshore',
+                'forecasts':
+                [
+                    { 'Description': 'Lake Ontario', 'url': '/great_lakes/LO/LOZ061.txt' },
+                    { 'Description': 'Niagara River to Hamlin Beach NY beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LO/LOZ062.txt' },
+                    { 'Description': 'Hamlin Beach to Sodus Bay NY beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LO/LOZ063.txt' },
+                    { 'Description': 'Sodus Bay to Mexico Bay NY beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LO/LOZ064.txt' },
+                    { 'Description': 'Mexico Bay NY to the St. Lawrence River beyond 5 NM offshore to U.S./Canadian border', 'url': '/great_lakes/LO/LOZ065.txt' }
+                ]
+            }]
+         }
     ]
 }]
 
--- weather-mvc/MarineForecast/marine.js
+++ weather-mvc/MarineForecast/marine.js
@@ -1,14 +1,178 @@
 ﻿var Weather = {};
 
 Weather.Marine = (function () {
+    var currentLocation = {};
+    var currentZone = {};
+    currentSubZone = {};
+
     function init() {
-        alert('init');
+        setMainScreen();
     };
 
-    
+    function setMainScreen() {
+        //Reset any previous browses
+        currentLocation = {};
+        currentZone = {};
+        currentSubZone = {};
+
+        //locations is passed in the locations.js file (which needs to be loaded before we get here)
+        var _mainContainer = $("#mainContainer");
+        _mainContainer.empty();
+        _mainContainer.append("<label class='words_big' >Please Select an Area</label><hr/>");
+        for (var i = 0; i < locations.length; i++) {
+            //var loc = locations[i];
+            var loc = jQuery.extend(true, {}, locations[i]);
+            var _locationButton = $('<button/>', {
+                type: 'button',
+                name: 'locationButton',
+                text: loc.title,
+                index: i,
+                value: i,
+                on: {
+                    click: function () {
+                        locationButtonClicked(this.value);
+                    }
+                }
+            });
+            _mainContainer.append(_locationButton);
+            _mainContainer.append("<br/>");
+        };
+    };
+
+    function setLocationScreen(location) {
+        var _mainContainer = $("#mainContainer");
+        _mainContainer.empty();
+        _mainContainer.append(makeBackButton());
+        _mainContainer.append("<label class='words_med' >" + location.title + "</label><hr/>");
+        for (var i = 0; i < location.zones.length; i++) {
+            var zone = location.zones[i];
+            var _zoneButton = $('<button/>', {
+                type: 'button',
+                name: 'zoneButton',
+                text: zone.title,
+                value: i,
+                on: {
+                    click: function () {
+                        zoneButtonClicked(this.value);
+                    }
+                }
+            });
+            _mainContainer.append(_zoneButton);
+            _mainContainer.append("<br/>");
+        }
+    }
+
+    function setZoneScreen(zone) {
+        var _mainContainer = $("#mainContainer");
+        _mainContainer.empty();
+        _mainContainer.append(makeBackButton());
+        _mainContainer.append("<label class='words_med' >" + currentLocation.title + "->" + zone.title + "</label><hr/>");
+        for (var i = 0; i < zone.subZones.length; i++) {
+            var subZone = zone.subZones[i];
+            var _subZoneButton = $('<button/>', {
+                type: 'button',
+                name: 'subZoneButton',
+                text: subZone.title,
+                value: i,
+                on: {
+                    click: function () {
+                        subZoneButtonClicked(this.value);
+                    }
+                }
+            });
+            _mainContainer.append(_subZoneButton);
+            _mainContainer.append("<br/>");
+        }
+    }
+
+    function setSubZoneScreen(subZone) {
+        var _mainContainer = $("#mainContainer");
+        _mainContainer.empty();
+        _mainContainer.append(makeBackButton());
+        _mainContainer.append("<label class='words_med' >" + currentLocation.title + "->" + currentZone.title + "->" + subZone.title + "</label><hr/>");
+        for (var i = 0; i < subZone.forecasts.length; i++) {
+            var forecast = subZone.forecasts[i];
+            var _forecastButton = $('<button/>', {
+                type: 'button',
+                name: 'foreButton',
+                text: forecast.Description,
+                value: i,
+                on: {
+                    click: function () {
+                        forecastButtonClicked(this.value);
+                    }
+                }
+            });
+            _mainContainer.append(_forecastButton);
+            _mainContainer.append("<br/>");
+        }
+    }
 
 
-//Any functions declared in this 'return' function act like public functions 
+    function setForecastScreen(forecast) {
+        var _mainContainer = $("#mainContainer");
+        _mainContainer.empty();
+        _mainContainer.append(makeBackButton());
+        _mainContainer.append("<label class='words_med' >" + forecast.Description + "</label><hr/>");
+
+        _mainContainer.append(forecast.url);
+
+        var words = getForecast(forecast.url);
+        _mainContainer.append(words);
+    }
+
+    function makeBackButton() {
+        var backButton = $('<button/>', {
+            type: 'button',
+            id: 'backButton',
+            text: 'Start Over',
+            click: function () {
+                setMainScreen();
+            }
+        });
+        return backButton;
+    }
+
+    function getForecast(url) {
+        var _fullUrl = "http://weather.noaa.gov/cgi-bin/fmtbltn.pl?file=forecasts/marine" + url;
+
+        var _iframe = $('<iframe>', {
+            src: _fullUrl,
+            width: '90%',
+            height: '100%'
+        });
+        return _iframe;
+    }
+
+
+    //Events
+    function backButtonClicked() {
+        this.init();
+    }
+
+    function locationButtonClicked(index) {
+        currentLocation = locations[index];
+        currentZone = {};
+        currentSubZone = {};
+        setLocationScreen(currentLocation);
+    }
+
+    function zoneButtonClicked(index) {
+        currentZone = currentLocation.zones[index];
+        currentSubZone = {};
+        setZoneScreen(currentZone);
+    }
+
+    function subZoneButtonClicked(index) {
+        currentSubZone = currentZone.subZones[index];
+        setSubZoneScreen(currentSubZone);
+    }
+
+    function forecastButtonClicked(index) {        
+        setForecastScreen(currentSubZone.forecasts[index]);
+    }
+
+//Any functions declared in this 'return' function act like public functions (all other functions are private)
     return {
         init: init
     }
